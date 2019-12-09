5c3568577dab40627bfbab43df819424fd3e15b1
Laura Berge <lauraeberge@gmail.com>
2019-12-08 20:36:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+function map(array, func) {
+    let r = [];
+    array.forEach(e => r.push(func(e)));
+    return r;
+}
+
+function reduce(array, func, memo) {
+    memo = memo || array.shift();
+    array.forEach(e => memo = func(e, memo));
+    return memo;
+}
